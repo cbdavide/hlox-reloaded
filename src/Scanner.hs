bb226eefe419e0d20fde64c50c7d8e4ef6b6961c
David Castelblanco Benavides <cbdavides@gmail.com>
2025-03-23 15:50:01 -0500

    make some progress in the scanner

--- src/Scanner.hs
+++ src/Scanner.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE  OverloadedStrings #-}
+
 module Scanner (
-  TokenType (..)
+    scanTokens
 ) where
+
+import Control.Monad.State (State, gets, modify)
+import qualified Data.Text as T
 
 data TokenType =
     -- Single-character tokens
@@ -27,13 +32,102 @@ data TokenType =
 data Value = StringValue String | IntValue Int
     deriving (Eq, Show)
 
+
 data Token = Token
     { tokenType     :: TokenType
-    , lexeme        :: String
+    , lexeme        :: T.Text
     , literal       :: Value
 
-    -- Location information
-    , line          :: Int
-    , offset        :: Int
-    , length'       :: Int
+    -- Location info
+    , tokenOffset        :: Int
+    , tokenLegnth        :: Int
     } deriving (Show)
+
+
+data Error = Error
+    { errorOffset       :: Int
+    , errorLength       :: Int
+    } deriving (Show)
+
+
+data ScannerContext = ScannerContext
+    {
+    -- inputs
+      source        :: T.Text
+
+    -- state
+    , line          :: Int
+    , currentLexeme :: T.Text
+
+    -- outputs
+    , tokens        :: [Token]
+    , errors        :: [Error]
+    } deriving (Show)
+
+type ScannerState a = State ScannerContext a
+
+isAtEnd :: ScannerState Bool
+isAtEnd =  gets (T.null .source)
+
+isNotAtEnd :: ScannerState Bool
+isNotAtEnd = not <$> isAtEnd
+
+modifySource :: T.Text -> ScannerState ()
+modifySource s' = modify (\x -> x { source = s' })
+
+modifyTokens :: [Token] -> ScannerState ()
+modifyTokens t' = modify (\x -> x { tokens = t' })
+
+-- Take the next character from the source if any and update the context
+advance :: ScannerState (Maybe Char)
+advance = do
+    result <- gets (T.uncons . source)
+
+    case result of
+        Nothing -> return Nothing
+        Just (x, xs) -> modifySource xs >> return (Just x)
+
+-- Take the next character if it satisfies the given function
+advanceIfMatches :: (Char -> Bool) -> ScannerState Bool
+advanceIfMatches check = do
+    result <- gets (T.uncons . source)
+
+    case result of
+        Nothing -> return False
+        Just (x, _) -> if check x then advance >> return True else return False
+
+appendToken :: Token -> ScannerState ()
+appendToken t = gets tokens >>= (\x -> modifyTokens (x ++ [t]))
+
+createToken :: TokenType -> ScannerState Token
+createToken tp =
+    gets (Token tp . currentLexeme)
+    <*> return (StringValue "tmp")
+    <*> return 10
+    <*> gets (T.length . currentLexeme)
+
+addToken :: TokenType -> ScannerState ()
+addToken tp = createToken tp >>= appendToken
+
+processToken :: Char -> ScannerState ()
+processToken c = case c of
+    '(' -> addToken LEFT_PAREN
+    ')' -> addToken RIGHT_PAREN
+    '}' -> addToken LEFT_BRACE
+    '{' -> addToken RIGHT_BRACE
+    ',' -> addToken COMMA
+    '.' -> addToken DOT
+    '-' -> addToken MINUS
+    '+' -> addToken PLUS
+    ';' -> addToken SEMICOLON
+    '*' -> addToken STAR
+    _ -> undefined
+
+
+scanTokens :: ScannerState ()
+scanTokens = do
+    next <- advance
+
+    case next of
+        Nothing -> return ()
+        Just c -> processToken c >> scanTokens
