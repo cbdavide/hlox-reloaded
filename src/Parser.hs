11ef6291c717896305ca94e4f3726937e1b11ffb
David Castelblanco Benavides <cbdavides@gmail.com>
2025-04-19 11:26:52 -0500

    add parser for the 'primary' rule

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,20 +1,19 @@
-{-# LANGUAGE  OverloadedStrings #-}
+{-# LANGUAGE  OverloadedStrings, LambdaCase #-}
 
 module Parser (expression) where
 
-import Control.Monad.Except
-import Control.Monad.State (State, gets)
-import qualified Data.Text as T
-
-import Scanner (Token (..), TokenType (..))
-import Data.List (uncons)
+import Control.Monad (void)
+import Control.Monad.Except ( ExceptT, MonadError(..) )
 import Control.Monad.Extra (ifM)
+import Control.Monad.State (State, gets, modify)
+import Data.List (uncons)
+import qualified Data.Text as T
+import Scanner (Token (..), TokenType (..))
 
 data LiteralValue =
-      NumberValue Double
+      NumberValue Float
     | StringValue T.Text  
-    | TrueValue
-    | FalseValue
+    | BooleanValue Bool
     | Nil
     deriving (Eq, Show)
 
@@ -37,7 +36,10 @@ data ParseError = ParseError
 type Parser a = ExceptT ParseError (State ParserContext) a
 
 modifySource :: [Token] -> Parser ()
-modifySource = undefined
+modifySource ts = modify (\x -> x { source = ts })
+
+reportError :: T.Text -> Parser a
+reportError msg = peek >>= \mt -> throwError $ ParseError {errorMessage=msg, token=mt}
 
 advance :: Parser (Maybe Token)
 advance = do
@@ -57,10 +59,7 @@ unsafeAdvance  = do
      
     case tok of
         Just t -> return t
-        Nothing -> throwError $ ParseError 
-            { errorMessage="Unexpected end of tokens"
-            , token=Nothing
-            }
+        Nothing -> reportError "Unexpected end of tokens"
 
 peek :: Parser (Maybe Token)
 peek = gets (fmap fst . uncons . source)
@@ -87,11 +86,32 @@ unary :: Parser Expression
 unary = ifM (not <$> match [BANG, MINUS]) primary (Unary <$> unsafeAdvance <*> unary)
 
 primary :: Parser Expression
-primary = undefined
+primary = advance >>= \case
+        Nothing -> reportError "Expected expression"
+        Just t -> case tokenType t of
+            FALSE -> literal' (BooleanValue False)
+            TRUE -> literal' (BooleanValue True)
+            NIL -> literal' Nil
+            NUMBER -> literal' (NumberValue 2)
+            STRING -> literal' (StringValue "")
+            LEFT_PAREN -> group
+            _ -> reportError "Expected expression"
+
+literal' :: LiteralValue -> Parser Expression
+literal' l = return $ Literal l
+
+group :: Parser Expression
+group = do
+    expr <- expression
+    consume RIGHT_PAREN "Expected ')' after expression"
+    return $ Grouping expr
+
+consume :: TokenType -> T.Text -> Parser ()
+consume tp msg = ifM (match [tp]) (void advance) (reportError msg)
 
 many1 :: Parser Expression -> [TokenType] -> Parser Expression
 many1 rule tokenTypes = rule >>= go
-    where go expr = 
+    where go expr =
             ifM (not <$> match tokenTypes) 
                 (return expr) 
                 (Binary expr <$> unsafeAdvance <*> rule >>= go)
