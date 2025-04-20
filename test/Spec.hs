06ebe5193eb75ce6fb69018d4f5982e13b373070
David Castelblanco Benavides <cbdavides@gmail.com>
2025-04-20 11:33:37 -0500

    start adding unit tests for the parser

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,8 +2,9 @@ import Test.Tasty ( defaultMain, testGroup )
 import Test.Tasty.Hspec ( testSpecs )
 
 import ScannerSpec ( scannerSpecs )
+import ParserSpec ( parserSpecs )
 
 main :: IO ()
 main = do
-    specs <- concat <$> mapM testSpecs [scannerSpecs]
+    specs <- concat <$> mapM testSpecs [scannerSpecs, parserSpecs]
     defaultMain $ testGroup "All tests" [ testGroup "Specs" specs]
