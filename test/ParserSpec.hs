13e57ec4770fe65c80637ab6c56463ff76330999
David Castelblanco Benavides <cbdavides@gmail.com>
2025-04-21 20:56:45 -0500

    add unit tests for the unary rule

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE  OverloadedStrings #-}
+{-# LANGUAGE  OverloadedStrings, PatternSynonyms #-}
 
 module ParserSpec ( parserSpecs ) where
 
@@ -19,8 +19,20 @@ baseToken = Token
     , literal=EmptyValue
     }
 
+pattern BoolExpr :: Bool -> Expression
+pattern BoolExpr b = Literal (BooleanValue b)
+
+pattern NumExpr :: Float -> Expression
+pattern NumExpr n = Literal (Parser.NumberValue n)
+
+pattern StrExpr :: T.Text -> Expression
+pattern StrExpr t = Literal (Parser.StringValue t)
+
 createToken :: TokenType -> Token
 createToken tp = baseToken { tokenType = tp }
+
+createTokens :: [TokenType] -> [Token]
+createTokens tps = createToken <$> tps
 
 createNumericToken :: Float -> Token
 createNumericToken num = (createToken NUMBER) { literal = Scanner.NumberValue num }
@@ -39,16 +51,40 @@ parserSpecs = describe "Parser" $ do
 spec_parse :: Spec
 spec_parse = describe "parse" $ do
 
-    describe "unary expressions" $ do
+    describe "primary expressions" $ do
         
         let cases :: [(Token, Expression)]
-            cases = [ (createToken TRUE, Literal (BooleanValue True))
-                    , (createToken FALSE, Literal (BooleanValue False))
+            cases = [ (createToken TRUE, BoolExpr True)
+                    , (createToken FALSE, BoolExpr False)
                     , (createToken NIL, Literal Nil)
-                    , (createNumericToken 25, Literal (Parser.NumberValue 25))
-                    , (createStringToken "hello", Literal (Parser.StringValue "hello"))
+                    , (createNumericToken 25, NumExpr 25)
+                    , (createStringToken "hello", StrExpr "hello")
                     ]
 
         forM_ cases $ \(tp', expr') -> do
-            it ("success - parses '" ++ show (tokenType tp') ++ "' token") $ do
+            it ("success - parses '" ++ show (tokenType tp') ++ "' primary rule") $ do
                 parse [tp'] `shouldParseTo` expr'
+
+        it "success - parses grouping" $ do
+            let tokens = createTokens [LEFT_PAREN, TRUE, RIGHT_PAREN]
+                expectedExpr = Grouping (Literal $ BooleanValue True)
+
+            parse tokens `shouldParseTo` expectedExpr
+
+    describe "unary expressions" $ do
+
+        it "success - parses '!' unary rule" $ do
+            let operator = createToken BANG
+                tokens = operator : createTokens [TRUE]
+            parse tokens `shouldParseTo` Unary operator (BoolExpr True)
+
+        it "success - parses '-' unary rule" $ do
+            let operator = createToken MINUS
+                tokens = [operator, createNumericToken 10]
+            parse tokens `shouldParseTo` Unary operator (NumExpr 10)
+
+        it "success - parses nested unary rules" $ do
+            let operator1 = createToken BANG
+                operator2 = createToken MINUS
+                tokens = [operator1, operator2, createToken TRUE]
+            parse tokens `shouldParseTo` Unary operator1 (Unary operator2 (BoolExpr True))
