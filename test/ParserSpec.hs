163a3b0cdece68fbe4a7bfa4f2aca221230382d8
David Castelblanco Benavides <cbdavides@gmail.com>
2025-04-22 20:33:20 -0500

    add more parser unit tests

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -51,7 +51,7 @@ parserSpecs = describe "Parser" $ do
 spec_parse :: Spec
 spec_parse = describe "parse" $ do
 
-    describe "primary expressions" $ do
+    describe "primary rule" $ do
         
         let cases :: [(Token, Expression)]
             cases = [ (createToken TRUE, BoolExpr True)
@@ -62,29 +62,106 @@ spec_parse = describe "parse" $ do
                     ]
 
         forM_ cases $ \(tp', expr') -> do
-            it ("success - parses '" ++ show (tokenType tp') ++ "' primary rule") $ do
+            it ("success - parses '" ++ show (tokenType tp') ++ "' literal") $ do
                 parse [tp'] `shouldParseTo` expr'
 
-        it "success - parses grouping" $ do
+        it "success - parses grouped '(true)' expression" $ do
             let tokens = createTokens [LEFT_PAREN, TRUE, RIGHT_PAREN]
                 expectedExpr = Grouping (Literal $ BooleanValue True)
 
             parse tokens `shouldParseTo` expectedExpr
 
-    describe "unary expressions" $ do
+    describe "unary rule" $ do
 
-        it "success - parses '!' unary rule" $ do
+        it "success - parses '!true' expression" $ do
             let operator = createToken BANG
                 tokens = operator : createTokens [TRUE]
             parse tokens `shouldParseTo` Unary operator (BoolExpr True)
 
-        it "success - parses '-' unary rule" $ do
+        it "success - parses '-10' expression" $ do
             let operator = createToken MINUS
                 tokens = [operator, createNumericToken 10]
             parse tokens `shouldParseTo` Unary operator (NumExpr 10)
 
-        it "success - parses nested unary rules" $ do
+        it "success - parses nested '!-true' expression" $ do
             let operator1 = createToken BANG
                 operator2 = createToken MINUS
                 tokens = [operator1, operator2, createToken TRUE]
             parse tokens `shouldParseTo` Unary operator1 (Unary operator2 (BoolExpr True))
+
+    describe "factor rule" $ do
+
+        it "success - parses '100 * 1' expression" $ do
+            let operator = createToken STAR
+                tokens = [createNumericToken 100, operator, createNumericToken 25]
+            parse tokens `shouldParseTo` Binary (NumExpr 100) operator (NumExpr 25)
+
+        it "success - parses '2 / 0' expression" $ do
+            let operator = createToken SLASH
+                tokens = [createNumericToken 2, operator, createNumericToken 0]
+            parse tokens `shouldParseTo` Binary (NumExpr 2) operator (NumExpr 0)
+
+        it "success - parses '(1 * 2) / 3' expression" $ do
+            let operator1 = createToken STAR
+                operator2 = createToken SLASH
+
+                tokens = [ createNumericToken 1
+                         , operator1
+                         , createNumericToken 2
+                         , operator2
+                         , createNumericToken 3
+                         ]
+
+                expectedExpression = Binary
+                    (Binary (NumExpr 1) operator1 (NumExpr 2))
+                    operator2
+                    (NumExpr 3)
+
+            parse tokens `shouldParseTo` expectedExpression
+
+    describe "term rule" $ do
+
+        it "success - parses '22 + 12' expression" $ do
+            let operator = createToken PLUS
+                tokens = [createNumericToken 22, operator, createNumericToken 12]
+            parse tokens `shouldParseTo` Binary (NumExpr 22) operator (NumExpr 12)
+
+        it "success - parses '4 - 3' expression" $ do
+            let operator = createToken SLASH
+                tokens = [createNumericToken 4, operator, createNumericToken 3]
+            parse tokens `shouldParseTo` Binary (NumExpr 4) operator (NumExpr 3)
+
+        it "success - parses '1 + (2 / 3) + 5' expression" $ do
+            let sum' = createToken PLUS
+                rest' = createToken MINUS
+                div' = createToken SLASH
+
+                tokens = [ createNumericToken 1
+                         , sum'
+                         , createNumericToken 2
+                         , div'
+                         , createNumericToken 3
+                         , rest'
+                         , createNumericToken 5
+                         ]
+
+                expectedExpression = Binary
+                    (Binary (NumExpr 1) sum' (Binary (NumExpr 2) div' (NumExpr 3)))
+                    rest'
+                    (NumExpr 5)
+
+            parse tokens `shouldParseTo` expectedExpression
+
+    describe "comparison rule" $ do
+        let val1 = createNumericToken 10
+            val2 = createNumericToken 15
+            cases :: [(String, Token)]
+            cases = [ (">", createToken GREATER)
+                    , (">=", createToken GREATER_EQUAL)
+                    , ("<", createToken LESS)
+                    , ("<=", createToken LESS_EQUAL)
+                    ]
+
+        forM_ cases $ \(s', tp') -> do
+            it ("success - parses '10 " ++ s' ++ " 15' expression") $ do
+                parse [val1, tp', val2] `shouldParseTo` Binary (NumExpr 10) tp' (NumExpr 15)
