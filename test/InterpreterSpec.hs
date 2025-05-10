609836daf54006f4adb9ccf8d6937164e5770dcb
David Castelblanco Benavides <cbdavides@gmail.com>
2025-05-10 15:08:15 -0500

    add some unit tests for the interpreter

---
+++ test/InterpreterSpec.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE  OverloadedStrings #-}
+
+module InterpreterSpec ( interpreterSpecs ) where
+
+import Control.Monad ( forM_ )
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Interpreter (evalExpression)
+import Literal (LiteralValue (..))
+import Parser (Expression (Literal))
+
+interpreterSpecs :: Spec
+interpreterSpecs = describe "Interpreter" $ do
+    spec_eval_expression
+
+spec_eval_expression :: Spec
+spec_eval_expression = describe "evalExpression" $ do
+
+    describe "literal expression" $ do
+        let cases :: [(String, LiteralValue)]
+            cases = [ ("number", NumberValue 10)
+                    , ("string", StringValue "hello")
+                    , ("boolean", BooleanValue True)
+                    , ("nil", Nil)
+                    ]
+
+        forM_ cases $ \(name', val') -> do
+            it ("success - evals " ++ name' ++ " literal") $ do
+                evalExpression (Literal val') `shouldBe` Right val'
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,8 +3,9 @@ import Test.Tasty.Hspec ( testSpecs )
 
 import ScannerSpec ( scannerSpecs )
 import ParserSpec ( parserSpecs )
+import InterpreterSpec ( interpreterSpecs )
 
 main :: IO ()
 main = do
-    specs <- concat <$> mapM testSpecs [scannerSpecs, parserSpecs]
+    specs <- concat <$> mapM testSpecs [scannerSpecs, parserSpecs, interpreterSpecs]
     defaultMain $ testGroup "All tests" [ testGroup "Specs" specs]
